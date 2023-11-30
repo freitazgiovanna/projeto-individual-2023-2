1a539bad8be0f6f0b6777eee57be0a85d84dffea
freitazgiovanna <giovanna.bezerra@sptech.school>
2023-11-29 21:37:38 -0300

    python no jeito, só mandar pro sql server se deus quiser

--- python/projeto-individual-giovanna.py
+++ python/projeto-individual-giovanna.py
@@ -1,64 +1,127 @@
 import psutil
 import mysql.connector
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 
+# Função para autenticar o usuário
+def autenticar_usuario(mycursor, email, senha):
+    sql_query = "SELECT idUsuario FROM Usuario WHERE Email = %s AND Senha = %s"
+    mycursor.execute(sql_query, (email, senha))
+    result = mycursor.fetchone()
+    return result
+
+# Função para exibir a lista de ATMs disponíveis
+def exibir_atms_disponiveis(mycursor):
+    mycursor.execute("SELECT idATM, Modelo FROM ATM")
+    atms_disponiveis = mycursor.fetchall()
+    print("ATMs disponíveis para monitoramento:")
+    for atm in atms_disponiveis:
+        print(f"ATM ID: {atm[0]}, Modelo: {atm[1]}")
+
+# Função para obter informações do ATM
+def get_atm_info(mycursor, atm_id):
+    sql_query = "SELECT fkAgenciaEmp, AgenciaID FROM ATM WHERE idATM = %s"
+    mycursor.execute(sql_query, (atm_id,))
+    result = mycursor.fetchone()
+    return result
+
+# Função para obter o tempo de atividade do sistema
 def get_uptime():
-    # Obtém o tempo de inicialização do sistema
     uptime_seconds = psutil.boot_time()
-    
-    # Calcula a diferença entre o tempo atual e o tempo de inicialização
     uptime_timedelta = datetime.now() - datetime.fromtimestamp(uptime_seconds)
-    
     return uptime_timedelta
 
+# Função para formatar a diferença de tempo
 def format_timedelta(td):
-    # Formata a diferença de tempo para uma string mais legível
     days, seconds = td.days, td.seconds
     hours = seconds // 3600
     minutes = (seconds % 3600) // 60
     seconds = seconds % 60
-    
     return f"{days} dias, {hours} horas, {minutes} minutos, {seconds} segundos"
 
-while True:
-    # Tempo de atividade do sistema
-    uptime = get_uptime()
-    formatted_uptime = format_timedelta(uptime)
+# ...
 
-    # Conectar ao banco de dados MySQL
-    mydb = mysql.connector.connect(
-        host='localhost',
-        port='3306',
-        user='aluno',
-        password='sptech',
-        database='secureATM'
-    )
+# Conectar ao banco de dados
+mydb = mysql.connector.connect(host='localhost', port="3306", user="giba", password="fgandb25", database='secureATM')
 
-    if mydb.is_connected():
-        print("-----------------------------------------")
-        print(f"| Tempo de atividade do sistema: {formatted_uptime} |")
+if mydb.is_connected():
+    print("------------------------------------------------------------")
+    print("| Bem-vindo(a) à Secure ATM. Por favor, faça o login.       |")
+    print("------------------------------------------------------------")
+
+    # Solicitar e verificar e-mail e senha
+    email = input("Digite o seu e-mail: ")
+    senha = input("Digite a sua senha: ")
+
+    mycursor = mydb.cursor()
+    usuario_autenticado = autenticar_usuario(mycursor, email, senha)
+
+    if usuario_autenticado:
+        print("\nOlá! Selecione o ATM que você quer monitorar:")
         
-        # Criar um cursor
-        mycursor = mydb.cursor()
+        # Exibir ATMs disponíveis
+        exibir_atms_disponiveis(mycursor)
+        
+        # Permitir ao usuário escolher um ATM para monitoramento
+        atm_escolhido = input("Escolha o ATM pelo ID para monitorar:")
+        
+        # Obter informações do ATM escolhido
+        atm_info = get_atm_info(mycursor, atm_escolhido)
+        
+        if atm_info:
+            fk_AgenciaEmpresa, fk_ATMAgencia = atm_info
+            print("\nMonitoramento iniciado.")
 
-        idATM = 1  # Substitua pelo valor correto
-        ATMAgencia = 1  # Substitua pelo valor correto
-        AgenciaEmpresa = 1 # Substitua pelo valor correto
+            while True:
+                uptime = get_uptime()
+                formatted_uptime = format_timedelta(uptime)
 
-        # Inserir tempo de atividade no banco de dados
-        sql_query = """INSERT INTO tempoAtividade (atividade, fk__idATM, fk__ATMAgencia, fk__AgenciaEmpresa)
-            VALUES (%s, %s, %s, %s)"""
-        mycursor.execute(sql_query, (str(formatted_uptime), idATM, ATMAgencia, AgenciaEmpresa))
+                # Coleta de dados da máquina
+                cpu_percent = psutil.cpu_percent()
+                ram_percent = psutil.virtual_memory().percent
+                disk_percent = psutil.disk_usage('/').percent
 
-        # Fazer commit da transação
-        mydb.commit()
+                # Lista de variáveis que carregam os dados colhidos que serão inseridos nessa iteração EM ORDEM
+                dados_insert_cpu = [cpu_percent]
+                dados_insert_ram = [ram_percent]
+                dados_insert_disk = [disk_percent]
 
-        # Fechar o cursor e a conexão
-        mycursor.close()
-        mydb.close()
-    else:
-        print("Não foi possível conectar ao banco de dados.")
+                print("-----------------------------------------")
+                print(f"| Tempo de atividade do sistema: {formatted_uptime} |")
 
-    # Aguardar 5 segundos antes da próxima iteração
-    time.sleep(5)
+                # Abre cursor permitindo inserção
+                mycursor = mydb.cursor()
+
+                # Armazenar fks em variáveis para a inserção
+                fk_idATM = atm_escolhido
+                fk_ATMAgencia = fk_ATMAgencia
+                fk_AgenciaEmpresa = fk_AgenciaEmpresa
+
+                sql_query = """INSERT INTO tempoAtividade (atividade, fk__idATM, fk__ATMAgencia, fk__AgenciaEmpresa)
+                VALUES (%s, %s, %s, %s)"""
+                mycursor.execute(sql_query, (str(formatted_uptime), fk_idATM, fk_ATMAgencia, fk_AgenciaEmpresa))
+
+                # Inserção dados CPU
+                sql_query_cpu = "INSERT INTO leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (current_timestamp(), %s, 3, %s, 2)"
+                mycursor.execute(sql_query_cpu, (dados_insert_cpu[0], fk_idATM))
+                mydb.commit()
+
+                # Inserção dados RAM
+                sql_query_ram = "INSERT INTO leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (current_timestamp(), %s, 1, %s, 2)"
+                mycursor.execute(sql_query_ram, (dados_insert_ram[0], fk_idATM))
+                mydb.commit()
+
+                # Inserção dados DISCO
+                sql_query_disk = "INSERT INTO leitura (DataRegistro, Valor, Componente_ID, ATMComp_ID, APIID) VALUES (current_timestamp(), %s, 2, %s, 2)"
+                mycursor.execute(sql_query_disk, (dados_insert_disk[0], fk_idATM))
+                mydb.commit()
+
+                print(f"CPU: {cpu_percent}% | RAM: {ram_percent}% | Disco: {disk_percent}%")
+
+                # Aguarda 10 segundos antes da próxima coleta e inserção
+                time.sleep(10)
+else:
+    print("Não foi possível conectar ao banco de dados.")
+
+# Fechar a conexão com o banco
+mydb.close()
